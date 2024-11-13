fe8f7e473214f85dc7dbe40a3882d202f09aeeae
pfd1004 <pfd1004@alu.ubu.es>
2024-11-13 18:05:47 +0100

    getAlelos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Psicofarma")
+
+firebase = firebase.FirebaseApplication("https://psicofarma.streamlit.app/",None)
+
+def getAlelos(gen):
+    '''
+    Función que dado un gen proporciona todos los alelos encontrados para dicho gen. Para realizar esta consulta, emplea las guías del CPIC
+    
+    Parametros:
+        gen: String
+        Nombre del gen a buscar
+
+    Returns:
+        ListaFinalAlelos : list
+        Lista de alelos para dicho gen, obtenidos del CPIC
+    '''
+    url = "https://api.cpicpgx.org/v1/allele?genesymbol=eq."+ gen
+    
+    respuesta = requests.get(url)
+    datos = respuesta.json()
+
+    alelos = list()
+    
+    for i in range(len(datos)):
+        alelo = datos[i]["name"]
+        alelos.append(alelo)
+
+    setAlelos = set(alelos)
+    ListaFinalAlelos = (list(setAlelos))
+    ListaFinalAlelos.sort()
+    return ListaFinalAlelos
